f490279551d428888ccd7d65ce9bf34747d5e5cf
Jisu Park <jspark474@gmail.com>
2021-11-09 11:37:38 -0500

    Added open/close curve detection

--- sketch.js
+++ sketch.js
@@ -9,18 +9,20 @@ let input;
 let myCanvas; //for imported image
 let extraCanvas; //for drawing
 
-let brushSize = 3;
-let eraserSize = 3;
+let brushSize = 5;
+let eraserSize = 10;
 
 let arrUnity = [];
 
-const canvasSize = 512;
-const threshold = 253;
+const canvasSize = 500;
+const threshold = 250;
+//open cv
 
 function setup() {
     pixelDensity(1);    
     myCanvas = createCanvas(canvasSize, canvasSize);
     extraCanvas = createGraphics(canvasSize, canvasSize);
+    opencvCanvas = createGraphics(canvasSize, canvasSize);
     extraCanvas.clear();
     // canvas2 = createCanvas(canvasSize, canvasSize);
     // canvas2.clear();
@@ -99,7 +101,7 @@ function eraseStroke(){
     drawErase = false;
     extraCanvas.erase();
     extraCanvas.stroke(0);
-    extraCanvas.strokeWeight(3);
+    extraCanvas.strokeWeight(eraserSize);
     extraCanvas.noFill();
     extraCanvas.line(mouseX, mouseY, pmouseX, pmouseY);
 }
@@ -108,62 +110,89 @@ function saveImage(){
     saveCanvas(extraCanvas, 'MD_draw', 'jpg');
 }
 
-//currently checks the image with html ID #oimg
 function isClosed(){
-    // let imageBase64String = extraCanvas.elt.toDataURL("image/png", 1.0);
-    // imgElement = document.querySelector('#oimg');
-    // oimg.src = imageBase64String;
-    let src = cv.imread('oimg');
-    let dst = cv.Mat.zeros(src.rows, src.cols, cv.CV_8UC3);
-    cv.cvtColor(src, src, cv.COLOR_RGBA2GRAY, 0);
-    cv.threshold(src, src, 177, 200, cv.THRESH_BINARY);
-    let contours = new cv.MatVector();
-    let hierarchy = new cv.Mat();
-    cv.findContours(src, contours, hierarchy, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE);
-    let cnt = contours.get(0);
-    let hier;
-    // console.log(contours.size());
-    for (let i = 0; i < contours.size(); ++i) {
-            hier = hierarchy.intPtr(0, i)
-            // console.log(hier);
-            if (hier[2] >= 2){
-            console.log("This stroke DOES contain closed contour.");
-                src.delete(); dst.delete(); contours.delete(); hierarchy.delete(); cnt.delete();
-                return true;
-            }
-    }  
-    console.log("This stroke does NOT contain closed contour.");
-        src.delete(); dst.delete(); contours.delete(); hierarchy.delete(); cnt.delete();
+    let imageBase64String = opencvCanvas.elt.toDataURL('image/jpg', 1.0);
+    let canvas = document.getElementById('opencvCanvas');
+    let ctx = canvas.getContext('2d');
+    let msg = document.getElementById('msg');
+    let image = new Image();
+
+    image.onload = function() {
+        ctx.drawImage(image, 0, 0);   
+        let src = cv.imread('opencvCanvas')
+        cv.cvtColor(src, src, cv.COLOR_BGR2GRAY, 0);
+        cv.imshow('canvasOutput', src); 
+        cv.threshold(src, src, 177, 200, cv.THRESH_BINARY);
+        let contours = new cv.MatVector();
+        let hierarchy = new cv.Mat();
+        cv.findContours(src, contours, hierarchy, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE);
+        let cnt = contours.get(0);
+        let hier;
+        for (let i = 0; i < contours.size(); ++i) {
+                hier = hierarchy.intPtr(0, i)
+                if (hier[2] >= 2){
+                // console.log("Ready to be sent!");
+                    msg.innerHTML = "Ready to be sent";
+                    console.log(arrUnity);
+                    // src.delete(); dst.delete(); contours.delete(); hierarchy.delete(); cnt.delete();
+                    return true;
+                }
+        }  
+        // console.log("You need to create a closed shape");
+        msg.innerHTML = "You need to create a closed shape";
+        // src.delete(); dst.delete(); contours.delete(); hierarchy.delete(); cnt.delete();  
+    };
+    image.src = imageBase64String;
 }
+function clearPixels(){
+    for (let i = 0; i < height; i++) {
+        for (let j = 0; j < width; j++) {
+            var index = (i + j * width) * 4;
+                opencvCanvas.pixels[index]= 0;   //R
+                opencvCanvas.pixels[index+1]= 0; //G
+                opencvCanvas.pixels[index+2]= 0; //B
+                opencvCanvas.pixels[index+3]= 0; //A
+        }
+    }
+    opencvCanvas.updatePixels();
+
+}
+
 
 function createArray() {
     let imageBase64String = extraCanvas.elt.toDataURL("image/jpg", 1.0);    
     drawing = loadImage(imageBase64String, function done() {
-        extraCanvas.clear();
-        extraCanvas.image(drawing, 0, 0, width, height); // draw the image
-        extraCanvas.loadPixels();
+        // extraCanvas.clear();
+        opencvCanvas.image(drawing, 0, 0, width, height); // draw the image
+        opencvCanvas.loadPixels();
         for (let i = 0; i < height; i++) {
             for (let j = 0; j < width; j++) {
                 var index = (i + j * width) * 4;
-                if(extraCanvas.pixels[index+3]<=threshold){
-                    extraCanvas.pixels[index]= 255;   //R
-                    extraCanvas.pixels[index+1]= 255; //G
-                    extraCanvas.pixels[index+2]= 255; //B
-                    extraCanvas.pixels[index+3]= 255; //A
+                if(opencvCanvas.pixels[index+3]<=threshold){
+                    opencvCanvas.pixels[index]= 255;   //R
+                    opencvCanvas.pixels[index+1]= 255; //G
+                    opencvCanvas.pixels[index+2]= 255; //B
+                    opencvCanvas.pixels[index+3]= 255; //A
     
                 } else {
-                    extraCanvas.pixels[index]= 0;
-                    extraCanvas.pixels[index+1]= 0;
-                    extraCanvas.pixels[index+2]= 0;
-                    extraCanvas.pixels[index+3]= 255;
+                    opencvCanvas.pixels[index]= 0;
+                    opencvCanvas.pixels[index+1]= 0;
+                    opencvCanvas.pixels[index+2]= 0;
+                    opencvCanvas.pixels[index+3]= 255;
                 } 
-                arrUnity.push(extraCanvas.pixels[index]);
+                arrUnity.push(opencvCanvas.pixels[index]);
             }
         }
-          extraCanvas.updatePixels();
+        opencvCanvas.updatePixels();
+          isClosed();
+          clearPixels();
+
         //   saveImage();
         //   drawStat = false;
+        // console.log(arrUnity);
+        // saveImage();
       });  
+      
 }
 
 //button
@@ -172,4 +201,4 @@ clearButton.addEventListener("click", clearCanvas);
 penButton.addEventListener("click", drawStroke);
 eraseButton.addEventListener("click", eraseStroke);
 arrayButton.addEventListener("click", createArray);
-submitButton.addEventListener("click", isClosed);
+// submitButton.addEventListener("click", isClosed);
